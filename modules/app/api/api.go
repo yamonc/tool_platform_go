6fe97c1c6c5e70cf7170b5d4300d8728266404c9
ltyyz <1227150+ltyyz@users.noreply.github.com>
2020-09-17 05:42:53 +0000

    删除QueryForMap功能，gorm已自带

--- modules/app/api/api.go
+++ modules/app/api/api.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"biligo/modules/app/service"
-	"biligo/mysql"
 	"biligo/util"
 
 	"github.com/gin-gonic/gin"
@@ -16,14 +15,4 @@ import (
 //       200: Result
 func TestIndex(c *gin.Context) {
 	util.SuccessResult(service.TestService()).ToJSON(c)
-}
-
-// 测试 QueryForMap 函数
-func TestQueryForMap(c *gin.Context) {
-	maps, err := mysql.QueryForMap("select * from sys_user")
-	if err != nil {
-		util.FailResultWithMessage(err.Error(), nil).ToJSON(c)
-	} else {
-		util.SuccessResult(maps).ToJSON(c)
-	}
 }
